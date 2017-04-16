ef41235a188268ba933a12177fdb739b62eba673
MutikHidayati <mutikhidayati@gmail.com>
2017-04-16 09:29:43 +0700

    Version 0.1

---
+++ app/src/main/java/com/sibola/app/SignInActivity.java
@@ -0,0 +1,29 @@
+package com.sibola.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+
+    }
+
+
+    public void goToActivityNavigation (View view){
+        startActivity(new Intent (this, NavigationActivity.class));
+
+    }
+
+    public void goToActivitySignUp (View view){
+        startActivity(new Intent (this, SignUpActivity.class));
+
+    }
+
+}
